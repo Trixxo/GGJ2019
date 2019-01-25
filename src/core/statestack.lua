e268a4f330abe1ade75742b071dc4cded6c82e3b
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2019-01-25 22:59:43 +0100

    Drop some empty lines

--- src/core/statestack.lua
+++ src/core/statestack.lua
@@ -5,24 +5,21 @@ local function getStateStack()
     stateStack.states = {}
     stateStack.backCounter = 0
 
-
     function stateStack:current()
-        if self.states == 0 then return nil
-        
+        if self.states == 0 then
+            return nil
         elseif #self.states > 0 then
             return self.states[#self.states]
         end
     end
 
-
     function stateStack:push(element)
         table.insert(self.states, element)
-        self:current():load()   
+        self:current():load()
     end
 
-
     function stateStack:pop()
-        if self:current() then 
+        if self:current() then
             table.remove(self.states, #self.states)
         end
     end
@@ -33,7 +30,6 @@ local function getStateStack()
         end
         self:current():load()
     end
-     
 
     function stateStack:draw()
         for i = 0, #self.states-1 , 1 do
@@ -44,10 +40,9 @@ local function getStateStack()
             end
         end
         for i = self.backCounter, 0 , -1 do
-            self.states[#self.states-i]:draw() 
-        end 
+            self.states[#self.states-i]:draw()
+        end
     end
-
 
     function stateStack:update(dt)
         if self:current() then self:current():update(dt) end
