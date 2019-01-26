afd89d6c85bc16a87b8337918d80c33a6b38aa4c
Rafael Epplee <raffomania@gmail.com>
2019-01-26 16:44:47 +0100

    tune bg particles

--- src/entity/bgParticle.lua
+++ src/entity/bgParticle.lua
@@ -2,16 +2,16 @@ local function getBgParticle(x)
     local smoke = {}
     smoke.x = x
     smoke.particleSystem = love.graphics.newParticleSystem(resources.images.softCircle)
-    smoke.particleSystem:setEmissionRate(10)
+    smoke.particleSystem:setEmissionRate(3)
     smoke.particleSystem:setDirection(-math.pi/2)
     smoke.particleSystem:setSpeed(20, 150)
-    smoke.particleSystem:setParticleLifetime(30, 30)
+    smoke.particleSystem:setParticleLifetime(5, 8)
 
-    smoke.particleSystem:setSizes(0.3, 2.0)
+    smoke.particleSystem:setSizes(1.0, 1.5)
     smoke.particleSystem:setSizeVariation(0.5)
     smoke.particleSystem:setEmissionArea("uniform", 200, 0)
 
-    smoke.particleSystem:setColors(1, 1, 1, 0, 1, 1, 1, 0.5, 1, 1, 1, 0)
+    smoke.particleSystem:setColors(1, 1, 1, 0, 1, 1, 1, 0.3, 1, 1, 1, 0)
     smoke.particleSystem:start()
 
     smoke.body = love.physics.newBody(world, x, 900, "static")
