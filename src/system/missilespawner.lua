a3201ffc29e730c65ac0c0d5ea6703ce0478c652
Arne Beer <arne@twobeer.de>
2019-01-27 11:26:10 +0100

    Fix missile spawning logic

--- src/system/missilespawner.lua
+++ src/system/missilespawner.lua
@@ -14,9 +14,20 @@ local function getMissileSpawner()
             state = stack:current()
             local sw = settings.resolution.width
             local sh = settings.resolution.height
+
+            local relativeHeight = math.random(camera.y - 200, camera.y - 50)
+            if (relativeHeight < 0 and relativeHeight >= -sh * 0.5) or relativeHeight >= 0 then
+                y = relativeHeight
+                x = camera.x + sw/2 + math.random(sw * 0.1, sw * 0.3)
+            else
+                x = camera.x + sw + math.random(sw * 0.1, sw * 0.3)
+                y = -sh * 0.5
+            end
+
+            print(x, y)
             local randomspawn = {
-                x = camera.x + sw + math.random(sw * 0.1, sw * 0.3),
-                y = math.max(-sh/2, camera.y + math.random(-sh/2, sh/2))
+                x = x,
+                y = y,
             }
             local new_missile = getMissile(randomspawn.x, randomspawn.y)
             -- print("Spawning missile at ", randomspawn.x, randomspawn.y)
